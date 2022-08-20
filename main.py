38a0fa9e47f4addedb02eaa564339ab510508217
oadeniran <oadeniran82@gmail.com>
2022-08-21 00:59:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import gc
 
 app = Flask(__name__)
 app.secret_key = 'team-deep-learning'
+
 class encode_columns(BaseEstimator, TransformerMixin):
     def __init__(self, columns = None):
         self.columns = columns
@@ -43,9 +44,9 @@ class encode_columns(BaseEstimator, TransformerMixin):
                 val_dict = encoding[col]
             X[col] = X[col].map(val_dict)
         return X
+
 with open('final_model.pickle', 'rb') as f:
     model = pickle.load(f)
-
 
 
 
@@ -71,4 +72,40 @@ def help():
     
 
 if __name__ == "__main__":
+    
+    class encode_columns(BaseEstimator, TransformerMixin):
+        def __init__(self, columns = None):
+            self.columns = columns
+        
+        def fit(self, X, y = None):
+            return self
+        def transform(self, X, y = None):
+            cols_to_transform = list(X.columns)
+            if self.columns:
+                cols_to_transform  = self.columns
+            encoding = {
+                'Classification_Size' : {
+                    'XL' : 1,
+                    'L' : 2,
+                    'M' : 3,
+                    'S' : 4},
+                'Research_Intensity' : {
+                    'VH' : 1,
+                    'HI' : 2,
+                    'MD' : 3,
+                    'LO' : 4},
+                'Status' : {
+                    'A' : 1,
+                    'B' : 2,
+                    'C' : 3}
+                }
+            for col in cols_to_transform:
+                if col not in list(encoding.keys()):
+                    val_dict = {v: i for i, v in enumerate(np.unique(X[col]))}
+                    #print(val_dict)
+                else:
+                    val_dict = encoding[col]
+                X[col] = X[col].map(val_dict)
+            return X
+
     app.run(debug = False)
